fbadfb4497b28c3e316da7cc1967d79c4e16b248
José Augusto <guto.machoski@gmail.com>
2022-04-16 17:44:59 -0300

    Utilizando postgres para armazenamento e tsyringe para injecao

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,3 +1,5 @@
+import { inject, injectable } from "tsyringe";
+
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 interface IRequest {
@@ -5,11 +7,15 @@ interface IRequest {
   description: string;
 }
 
+@injectable()
 class CreateCategoryUseCase {
-  constructor(private categoriesRepositoy: ICategoriesRepository) {}
+  constructor(
+    @inject("CategoriesRepository")
+    private categoriesRepositoy: ICategoriesRepository,
+  ) {}
 
-  execute({ name, description }: IRequest): void {
-    const categoryAlreadyExists = this.categoriesRepositoy.findByName(name);
+  async execute({ name, description }: IRequest): Promise<void> {
+    const categoryAlreadyExists = await this.categoriesRepositoy.findByName(name);
 
     if (categoryAlreadyExists) {
       throw new Error("Category already exists");
