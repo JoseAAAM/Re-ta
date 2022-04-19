903a2b4ceb62a316429f0867d307da08e54f6b63
José Augusto <guto.machoski@gmail.com>
2022-04-19 00:06:38 -0300

    Criando tabela usuarios e rota de criacao de usuario, lidando com jwt e criptografia de senha

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 
+import { authenticateRoutes } from "./authenticate.routes";
+import { userRoutes } from "./users.routes";
 import { categoriesRoutes } from "./categories.routes";
 import { specificationsRoutes } from "./specifications.routes";
 
@@ -7,5 +9,7 @@ const router = Router();
 
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationsRoutes);
+router.use("/users", userRoutes);
+router.use(authenticateRoutes);
 
 export { router };
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -4,7 +4,11 @@ import { ICategoriesRepository } from "../../modules/cars/repositories/ICategori
 import { CategoriesRepository } from "../../modules/cars/repositories/implementations/categoriesRepository";
 import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
 import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
+import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
 
 container.registerSingleton<ICategoriesRepository>("CategoriesRepository", CategoriesRepository);
 
 container.registerSingleton<ISpecificationsRepository>("SpecificationsRepository", SpecificationsRepository);
+
+container.registerSingleton<IUsersRepository>("UsersRepository", UsersRepository);
